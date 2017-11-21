89e8939fd5e46bb57fced7145f8c15da2d2fe57e
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-11-21 16:27:01 +0100

    Analytische vergelijking

--- Taak1/test_algorithms.m
+++ Taak1/test_algorithms.m
@@ -1,12 +1,14 @@
 clear all
 close all
 % initializations
-    tf = 0.065; % time window length
+   %tf = 0.065; % time window length
+    tf = 1/pi^2;
     nx = 20;       
     nt = 100;
     y = linspace(0,1,nx+1);
-    ic(1:nx/2) = y(1:nx/2); %sin(20*pi*x.^2.*(1-x)); % initial condition (MATLAB syntax!)
-    ic(nx/2+1:nx+1) = 1-y(nx/2+1:nx+1); %sin(20*pi*x.^2.*(1-x)); % initial condition (MATLAB syntax!)
+%     ic(1:nx/2) = y(1:nx/2); %sin(20*pi*x.^2.*(1-x)); % initial condition (MATLAB syntax!)
+%     ic(nx/2+1:nx+1) = 1-y(nx/2+1:nx+1); %sin(20*pi*x.^2.*(1-x)); % initial condition (MATLAB syntax!)
+    ic(:)= linspace(1,1, nx+1);
     f1= @(t)0;                  % lhs Dirichlet condition
     f2= @(t)0;                  % rhs Dirichlet condition
     figure
@@ -15,21 +17,21 @@ close all
         subplot(3,2,1+i*5); 
         plot(xe,ue),title(sprintf('t=%4.3f',i*tf));
     end
-    suptitle('Explicit Euler');
+    %suptitle('Explicit Euler');
     figure
     for i = 0:0.2:1        
         [xi,ui] = Implicit_Euler(i*tf,nx,i*nt,f1,f2,ic);
         subplot(3,2,1+i*5); 
         plot(xi,ui),title(sprintf('t=%4.3f',i*tf));
     end
-    suptitle('Implicit Euler');
+    %suptitle('Implicit Euler');
     figure
     for i = 0:0.2:1        
         [xcn,ucn] = Crank_Nicolson(i*tf,nx,i*nt,f1,f2,ic);
         subplot(3,2,1+i*5); 
         plot(xcn,ucn),title(sprintf('t=%4.3f',i*tf));
     end
-    suptitle('Crank Nicolson');
+    %suptitle('Crank Nicolson');
     
     %relatieve fouten
     figure
