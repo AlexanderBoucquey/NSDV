e6b0ee974f0b3b19fa4b43a00c4f5e34cd8923bb
Mattias <Gebruiker@10.43.72.255>
2017-12-18 20:12:14 +0100

    checks

--- Taak2/Taak2.m
+++ Taak2/Taak2.m
@@ -2,24 +2,23 @@ clear all
 close all
 % initializations
    %tf = 0.065; % time window length
-    tf = 1;
+    tf = 0.3183;
     nx = 40;  
     ny = 40;
     nt = 10000;
-    nf = 300;
+    nf = 100;
     [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
-    ic_heat=ones(nx+1,ny+1,1);  % Begin voorwaarde warmtevgl
-    ic_wave(1:nx+1,1:ny+1)=sin(pi*x')*sin(pi*y);    % Bvw Golfvgl
-   ic_transp=15*(x-x.^2).*(y-y.^2).*exp(50*((x-0.5)^2+(y-0.5)^2)); 
+    ic_1=ones(nx+1,ny+1,1);  % Begin voorwaarde warmtevgl
+    ic_2=sin(pi*x).*sin(pi*y);    % Bvw Golfvgl
+   ic_3=15*(x-x.^2).*(y-y.^2).*exp(50*((x-0.5)^2+(y-0.5)^2)); 
     f1= @(t)0;                  % lhs Dirichlet condition
     f2= @(t)0;                  % rhs Dirichlet condition
     f3= @(t)0;                  % uhs Dirichlet condition
     f4= @(t)0;                  % abhs Dirichlet condition
     figure('Renderer','zbuffer');
-    [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
-   [xu,yu, uu] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
-   [xt,yt, eut] = Explicit_Euler_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
-   [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
+    %%
+    [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+   
 
     for k = 1:nf        
         surf(xe,ye,ue(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
@@ -28,38 +27,88 @@ close all
         ylabel('y','Rotation',0);
         zlabel('u','Rotation',0);
         M_e(k)=getframe;
+       
+    end
+    movie(M_e);
+    writerObj = VideoWriter('Explicit_euler_1.avi'); open(writerObj);
+    writeVideo(writerObj,M_e); close(writerObj);
+    
+    
+    %%
+    [xu,yu, uu] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+    for k = 1:nf        
+    
         surf(xu,yu,uu(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
-        zlim([-1 3]);
+        zlim([-1.5 1.5]);
         xlabel('x');
         ylabel('y','Rotation',0);
         zlabel('u','Rotation',0);
         M_u(k)=getframe;
-        surf(xt,yt,eut(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
-        zlim([-1 2]);
-        xlabel('x');
-        ylabel('y','Rotation',0);
-        zlabel('u','Rotation',0);
-        M_t(k)=getframe;
+       
+    end
+    
+    
+    movie(M_u);
+    writerObj = VideoWriter('Explicit_euler_2.avi'); open(writerObj);
+    writeVideo(writerObj,M_u); close(writerObj);
+  %%  
+%    [xt,yt, eut] = Explicit_Euler_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
+%     
+%     for k = 1:nf        
+%        
+%         surf(xt,yt,eut(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
+%         zlim([-1 2]);
+%         xlabel('x');
+%         ylabel('y','Rotation',0);
+%         zlabel('u','Rotation',0);
+%         M_t(k)=getframe;
+%       
+%     end
+%     
+%     
+%     
+%     movie(M_t);
+%     writerObj = VideoWriter('Explicit_euler_3.avi'); open(writerObj);
+%     writeVideo(writerObj,M_t); close(writerObj);
+%    
+%     %%
+%        [xuh,yuh, uh] = Upwind_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
+% 
+%     for k = 1:nf        
+%          surf(xuh,yuh, uh(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
+%         zlim([0 1]);
+%         xlabel('x');
+%         ylabel('y','Rotation',0);
+%         zlabel('u','Rotation',0);
+%         M_uh(k)=getframe;
+%     end
+%     
+%     movie(M_uh);
+%     writerObj = VideoWriter('Upwind_1.avi'); open(writerObj);
+%     writeVideo(writerObj,M_uh); close(writerObj);
+%     
+    %%
+    
+    
+    
+       [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+
+    for k = 1:nf        
          surf(xut,yut,ut(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
-        zlim([-1 2]);
+        zlim([0 1]);
         xlabel('x');
         ylabel('y','Rotation',0);
         zlabel('u','Rotation',0);
         M_ut(k)=getframe;
     end
-    movie(M_e);
-    writerObj = VideoWriter('Explicit_euler_1.avi'); open(writerObj);
-    writeVideo(writerObj,M_e); close(writerObj);
-    movie(M_u);
-    writerObj = VideoWriter('Explicit_euler_2.avi'); open(writerObj);
-    writeVideo(writerObj,M_u); close(writerObj);
-    movie(M_t);
-    writerObj = VideoWriter('Explicit_euler_3.avi'); open(writerObj);
-    writeVideo(writerObj,M_t); close(writerObj);
+    
     movie(M_ut);
     writerObj = VideoWriter('Upwind_3.avi'); open(writerObj);
     writeVideo(writerObj,M_ut); close(writerObj);
-%     movie(M_u);
+
+    
+    
+    %     movie(M_u);
 %     writerObj = VideoWriter('Upwind_1.avi'); open(writerObj);
 %     writeVideo(writerObj,M_u); close(writerObj);
     %writerObj = VideoWriter(')
--- Taak2/Upwind_heat.m
+++ Taak2/Upwind_heat.m
@@ -1,7 +1,29 @@
-function [ output_args ] = Upwind_heat( input_args )
-%UPWIND_HEAT Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [x, y, u] = Upwind_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic)
+  %  with x in [0,1] and t in [0,tf]
+  %  ic is initial condition u(x,t=0)
+  dx = 1/nx; 
+  dy = 1/ny;
+  dt = tf/nt;     
+  x = linspace(0,1,nx+1);     % x-point coordinates
+  y = linspace(0,1,ny+1);     % y_point coordinates
+  mux = dt/(dx*dx); 
+  muy = dt/(dy*dy);
+  jx = 1:nx-1;    % indices of interior points:2..nx
+  jy = 1:ny-1;    % indices of interior points:2..ny
+                              % leftmost point has index 1
+                              % rightmost point has index nx+1
+  u(:,:,1) = ic;
+  k = 1;
+  for n = 1:nt
+       u(jx+1,jy+1,k+1) = mux*u(jx,jy+1,k) + (1-2*mux-2*muy)*u(jx+1,jy+1,k) + ...
+        mux*u(jx+2,jy+1,k)+ muy*u(jx+1,jy,k) + muy*u(jx+1,jy+2,k);
+    u(1,:,k+1) = f1(n*dt);
+    u(nx+1,:,k+1) = f2(n*dt); 
+    u(:,1,k+1) = f3(n*dt);
+    u(:,ny+1,k+1) = f4(n*dt) ; 
+    k = k +1;
+  end  
 end
+
+
 
