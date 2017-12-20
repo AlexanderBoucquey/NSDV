6f209c809807ea2472bfcaf1f593adaeae42b68c
Mattias <Gebruiker@10.43.72.255>
2017-12-20 14:11:34 +0100

    tests

--- Taak2/Taak2.m
+++ Taak2/Taak2.m
@@ -2,22 +2,24 @@ clear all
 close all
 % initializations
    %tf = 0.065; % time window length
-    tf = 0.3183;
+    tf = 0.1;
     nx = 40;  
     ny = 40;
-    nt = 10000;
+    nt = 626;
     nf = 100;
     [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
     ic_1=ones(nx+1,ny+1,1);  % Begin voorwaarde warmtevgl
     ic_2=sin(pi*x).*sin(pi*y);    % Bvw Golfvgl
    ic_3=15*(x-x.^2).*(y-y.^2).*exp(50*((x-0.5)^2+(y-0.5)^2)); 
+   ic_4=exp(-((x-0.5).^2 + (y-0.5).^2)/(1/4));
     f1= @(t)0;                  % lhs Dirichlet condition
     f2= @(t)0;                  % rhs Dirichlet condition
     f3= @(t)0;                  % uhs Dirichlet condition
     f4= @(t)0;                  % abhs Dirichlet condition
     figure('Renderer','zbuffer');
     %%
-    [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+  
+    [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
    
 
     for k = 1:nf        
@@ -35,7 +37,8 @@ close all
     
     
     %%
-    [xu,yu, uu] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+      
+    [xu,yu, uu] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
     for k = 1:nf        
     
         surf(xu,yu,uu(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
@@ -91,7 +94,7 @@ close all
     
     
     
-       [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+       [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
 
     for k = 1:nf        
          surf(xut,yut,ut(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
