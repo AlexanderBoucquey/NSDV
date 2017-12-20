ef6f97dfb35fc055f9cb86cdf1635dbaf980dc6a
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-12-20 16:24:54 +0100

    tussen fase

--- Taak2/Taak2.m
+++ Taak2/Taak2.m
@@ -1,59 +1,59 @@
-clear all
-close all
-% initializations
-   %tf = 0.065; % time window length
-    tf = 0.1;
-    nx = 40;  
-    ny = 40;
-    nt = 626;
-    nf = 100;
-    [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
-    ic_1=ones(nx+1,ny+1,1);  % Begin voorwaarde warmtevgl
-    ic_2=sin(pi*x).*sin(pi*y);    % Bvw Golfvgl
-   ic_3=15*(x-x.^2).*(y-y.^2).*exp(50*((x-0.5)^2+(y-0.5)^2)); 
-   ic_4=exp(-((x-0.5).^2 + (y-0.5).^2)/(1/4));
-    f1= @(t)0;                  % lhs Dirichlet condition
-    f2= @(t)0;                  % rhs Dirichlet condition
-    f3= @(t)0;                  % uhs Dirichlet condition
-    f4= @(t)0;                  % abhs Dirichlet condition
-    figure('Renderer','zbuffer');
+% clear all
+% close all
+% % initializations
+%    %tf = 0.065; % time window length
+%     tf = 0.1;
+%     nx = 40;  
+%     ny = 40;
+%     nt = 626;
+%     nf = 100;
+%     [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
+%     ic_1=ones(nx+1,ny+1,1);  % Begin voorwaarde warmtevgl
+%     ic_2=sin(pi*x).*sin(pi*y);    % Bvw Golfvgl
+%    ic_3=15*(x-x.^2).*(y-y.^2).*exp(50*((x-0.5)^2+(y-0.5)^2)); 
+%    ic_4=exp(-((x-0.5).^2 + (y-0.5).^2)/(1/4));
+%     f1= @(t)0;                  % lhs Dirichlet condition
+%     f2= @(t)0;                  % rhs Dirichlet condition
+%     f3= @(t)0;                  % uhs Dirichlet condition
+%     f4= @(t)0;                  % abhs Dirichlet condition
+%     figure('Renderer','zbuffer');
     %%
-  
-    [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
-   
-
-    for k = 1:nf        
-        surf(xe,ye,ue(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
-        zlim([0 1]);
-        xlabel('x');
-        ylabel('y','Rotation',0);
-        zlabel('u','Rotation',0);
-        M_e(k)=getframe;
-       
-    end
-    movie(M_e);
-    writerObj = VideoWriter('Explicit_euler_1.avi'); open(writerObj);
-    writeVideo(writerObj,M_e); close(writerObj);
+%   
+%     [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
+%    
+% 
+%     for k = 1:nf        
+%         surf(xe,ye,ue(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
+%         zlim([0 1]);
+%         xlabel('x');
+%         ylabel('y','Rotation',0);
+%         zlabel('u','Rotation',0);
+%         M_e(k)=getframe;
+%        
+%     end
+%     movie(M_e);
+%     writerObj = VideoWriter('Explicit_euler_1.avi'); open(writerObj);
+%     writeVideo(writerObj,M_e); close(writerObj);
     
     
     %%
       
-    [xu,yu, uu] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
-    for k = 1:nf        
-    
-        surf(xu,yu,uu(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
-        zlim([-1.5 1.5]);
-        xlabel('x');
-        ylabel('y','Rotation',0);
-        zlabel('u','Rotation',0);
-        M_u(k)=getframe;
-       
-    end
-    
-    
-    movie(M_u);
-    writerObj = VideoWriter('Explicit_euler_2.avi'); open(writerObj);
-    writeVideo(writerObj,M_u); close(writerObj);
+%     [xu,yu, uu] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
+%     for k = 1:nf        
+%     
+%         surf(xu,yu,uu(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
+%         zlim([-1.5 1.5]);
+%         xlabel('x');
+%         ylabel('y','Rotation',0);
+%         zlabel('u','Rotation',0);
+%         M_u(k)=getframe;
+%        
+%     end
+%     
+%     
+%     movie(M_u);
+%     writerObj = VideoWriter('Explicit_euler_2.avi'); open(writerObj);
+%     writeVideo(writerObj,M_u); close(writerObj);
   %%  
 %    [xt,yt, eut] = Explicit_Euler_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_transp);
 %     
@@ -93,21 +93,21 @@ close all
     %%
     
     
-    
-       [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
-
-    for k = 1:nf        
-         surf(xut,yut,ut(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
-        zlim([0 1]);
-        xlabel('x');
-        ylabel('y','Rotation',0);
-        zlabel('u','Rotation',0);
-        M_ut(k)=getframe;
-    end
-    
-    movie(M_ut);
-    writerObj = VideoWriter('Upwind_3.avi'); open(writerObj);
-    writeVideo(writerObj,M_ut); close(writerObj);
+%     
+%        [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_3);
+% 
+%     for k = 1:nf        
+%          surf(xut,yut,ut(:,:,k*floor(nt/nf))),title(sprintf('t=%4.3f',tf*k/nf));
+%         zlim([0 1]);
+%         xlabel('x');
+%         ylabel('y','Rotation',0);
+%         zlabel('u','Rotation',0);
+%         M_ut(k)=getframe;
+%     end
+%     
+%     movie(M_ut);
+%     writerObj = VideoWriter('Upwind_3.avi'); open(writerObj);
+%     writeVideo(writerObj,M_ut); close(writerObj);
 
     
     
@@ -115,3 +115,76 @@ close all
 %     writerObj = VideoWriter('Upwind_1.avi'); open(writerObj);
 %     writeVideo(writerObj,M_u); close(writerObj);
     %writerObj = VideoWriter(')
+%% Oef3
+clear variables;
+close all;
+
+tf = 0.1;
+% nx = 40;  
+% ny = 40;
+% nt = 626;
+% nf = 100;
+f1= @(t)0;                  % lhs Dirichlet condition
+f2= @(t)0;                  % rhs Dirichlet condition
+f3= @(t)0;                  % uhs Dirichlet condition
+f4= @(t)0;                  % abhs Dirichlet condition
+
+i = 1;
+max_err = zeros(50/5-1,1);
+for nx = 10:5:50
+    ny = nx;
+    nt = ((nx/10)^2*500);
+    [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
+    ic_2=sin(pi*x).*sin(pi*y);  % Bvw 
+    u_exact_heat = @(x,y,t) sin(pi*x)*sin(pi*y)*exp(-2*pi^2*t); 
+    [xe,ye, ue] = Explicit_Euler_heat(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+
+    for xn = 1:(nx/5-1)
+        x = xe(1,xn);
+        for yn = 1:(ny/5-1)
+            y = ye(1,yn);
+            err = abs(u_exact_heat(x,y,0.1) - ue(xn,yn,nt+1));
+            if (err > max_err(i))
+                max_err(i,1) = err;
+            end
+        end
+    end
+    i = i+1;
+end
+semilogy(linspace(10,50,9),max_err);
+
+i = 1;
+max_errw = zeros(100/5-1,1);
+max_errt = zeros(100/5-1,1);
+for nx = 10:5:100
+    ny = nx;
+    nt = (nx/10)*500;
+    [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
+    ic_2=sin(pi*x).*sin(pi*y);  % Bvw 
+    u_exact_wave = @(x,y,t) sin(pi*x)*sin(pi*y)*cos(sqrt(2)*pi*t);
+    u_exact_transport = @(x,y,t) sin(pi*(x+t))*sin(pi*(y+t));
+    [xw,yw, uw] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+    [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
+
+    for xn = 1:(nx/5-1)
+        x1 = xw(1,xn);
+        x2 = xut(1,xn);
+        for yn = 1:(ny/5-1)
+            y1 = yw(1,yn);
+            y2 = yut(1,yn);
+            errw = abs(u_exact_wave(x1,y1,0.1) - uw(xn,yn,nt+2));
+            errt = abs(u_exact_transport(x2,y2,0.1) - ut(xn,yn,nt+1));
+            if (errw > max_errw(i))
+                max_errw(i,1) = errw;
+            end
+            if (errt > max_errt(i))
+                max_errt(i,1) = errt;
+            end
+        end
+    end
+    i = i+1;
+end
+figure();
+semilogy(linspace(10,100,19),max_errw);
+figure();
+semilogy(linspace(10,100,19),max_errt);
