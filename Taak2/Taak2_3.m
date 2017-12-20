2e745c5a6699774c70456be54048cbc4828e8973
Alexander Boucquey <alexander.boucquey@student.kuleuven.be>
2017-12-20 18:41:49 +0100

    transportok

--- Taak2/Taak2_3.m
+++ Taak2/Taak2_3.m
@@ -3,10 +3,6 @@ clear variables;
 close all;
 
 tf = 0.1;
-% nx = 40;  
-% ny = 40;
-% nt = 626;
-% nf = 100;
 f1= @(t)0;                  % lhs Dirichlet condition
 f2= @(t)0;                  % rhs Dirichlet condition
 f3= @(t)0;                  % uhs Dirichlet condition
@@ -30,20 +26,19 @@ semilogy(linspace(10,50,9),max_err);
 i = 1;
 max_errw = zeros(100/5-1,1);
 max_errt = zeros(100/5-1,1);
-for nx = 10:5:100
+for nx = 10:10:100
     t = tf;
     ny = nx;
     nt = (nx/10)*500;
     [x,y]=meshgrid((0:nx)/(nx),(0:ny)/(ny));    
     ic_2=sin(pi*x).*sin(pi*y);  % Bvw 
     u_exact_wave = sin(pi*x).*sin(pi*y)*cos(sqrt(2)*pi*t);
-    u_exact_transport = sin(pi*(x+t)).*sin(pi*(y+t));
-    u_exact_transport(ceil(9/10*nx):nx+1,:) = zeros(floor(1/10*nx)+2,nx+1);
-    u_exact_transport(:,ceil(9/10*ny):ny+1) = zeros(ny+1,floor(1/10*ny)+2);
+    u_exact_transport=zeros(nx,ny);
+    u_exact_transport(1:0.9*nx,1:0.9*ny) = ic_2(0.1*nx+1:nx,0.1*ny+1:ny);
     [xw,yw, uw] = Explicit_Euler_wave(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
     [xut,yut, ut] = Upwind_transp(tf,nx,ny,nt,f1,f2,f3,f4,ic_2);
     max_errw(i) = max(max(abs(u_exact_wave-uw(:,:,nt+2))));
-    max_errt(i) = max(max(abs(u_exact_transport-ut(:,:,nt+1))));
+    max_errt(i) = max(max(abs(u_exact_transport(2:end,2:end)-ut(2:nx,2:ny,nt+1))));
     i = i+1;
 end
 figure();
